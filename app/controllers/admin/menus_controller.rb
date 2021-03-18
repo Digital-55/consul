5dd3f2aded6486f2f646b69d879af950d1b3e5c8
jrsahuquillo <jrsahuquillo@gmail.com>
2021-03-18 10:34:40 +0100

    Creates menus

--- app/controllers/admin/menus_controller.rb
+++ app/controllers/admin/menus_controller.rb
@@ -7,4 +7,22 @@ class Admin::MenusController < Admin::BaseController
     @menus =  Menu.send(@current_filter).page(params[:page])
   end
 
+  def new
+    @menu = Menu.new
+  end
+
+  def create
+    @menu = Menu.new(menu_params)
+    if @menu.save
+      redirect_to admin_menus_path, notice: t("admin.menus.create.notice")
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def menu_params
+    params.require(:menu).permit(:title, :section, :enabled)
+  end
 end
