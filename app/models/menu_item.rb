23af0fa9132a3fca1f9422e21ddc3e0d94608883
jrsahuquillo <jrsahuquillo@gmail.com>
2021-04-13 09:47:25 +0200

    Displays red border when field error validation

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -4,6 +4,25 @@ class MenuItem < ApplicationRecord
   accepts_nested_attributes_for :menu_items, allow_destroy: true, reject_if: :all_blank
 
   validates :title, presence: true
+  validate :url_format
+  validate :title_format
 
   scope :enabled, -> { where(disabled: false) }
+
+  def title_format
+    self.errors.add :title, "format is invalid!" unless title_valid?
+  end
+
+  def title_valid?
+    !!title.match(/\A[a-zA-Z0-9 ]*\z/)
+  end
+
+  def url_format
+    self.errors.add :url, "format is invalid!" unless url_valid? || url.blank?
+  end
+
+  def url_valid?
+    !!url.match(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-=\?]*)*\/?$/) &&
+    !!url.match(URI::regexp)
+  end
 end
