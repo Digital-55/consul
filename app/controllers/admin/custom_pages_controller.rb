125a9887bd836c245b0bc262bc7a8bdd1406ae40
jrsahuquillo <jrsahuquillo@gmail.com>
2021-04-16 11:44:37 +0200

    Allows to create custom_pages

--- app/controllers/admin/custom_pages_controller.rb
+++ app/controllers/admin/custom_pages_controller.rb
@@ -6,4 +6,23 @@ class Admin::CustomPagesController < Admin::BaseController
   def index
     @custom_pages = CustomPage.send(@current_filter).page(params[:page])
   end
+
+  def new
+    @custom_page = CustomPage.new
+  end
+
+  def create
+    @custom_page = CustomPage.new(custom_page_params)
+    if @custom_page.save
+      redirect_to edit_admin_custom_page_path(@custom_page), notice: t("admin.custom_page.create.notice")
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def custom_page_params
+    params.require(:custom_page).permit(:title, :slug, :published)
+  end
 end
--- app/models/custom_page.rb
+++ app/models/custom_page.rb
@@ -1,4 +1,6 @@
 class CustomPage < ApplicationRecord
+  validates :slug, uniqueness: true
+
   scope :published, -> { where(published: true) }
   scope :draft, -> { where(published:false) }
   scope :sorted, -> { order(updated_at: :desc) }
