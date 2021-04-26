106a6efaecd4a428f2b39f894050d3e8f123ed0f
jrsahuquillo <jrsahuquillo@gmail.com>
2021-04-26 09:54:53 +0200

    Improves rich_text module sorting effect

--- app/models/rich_text_module.rb
+++ app/models/rich_text_module.rb
@@ -1,7 +1,6 @@
 class RichTextModule < CustomPageModule
   belongs_to :custom_page
   TEXT_MAX_LENGTH = 5000
-  validates :rich_text, presence: true
-  validates :rich_text, length: { maximum: TEXT_MAX_LENGTH }
+  validates :rich_text, presence: true, length: { maximum: TEXT_MAX_LENGTH }
 
 end
