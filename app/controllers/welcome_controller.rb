ad5b8d91094c3c0398f6db67da6fd53f5a96b535
Antonio Javier Moreno Hernández <ajmorenoh@indra.es>
2021-05-03 15:28:43 +0200

    [DEMAD-SG] Desarrollo de funcionalidades

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -48,6 +48,7 @@ class WelcomeController < ApplicationController
 
   def generic_search
     @orders_generic = Sg::Setting.order_settings.active.order(id: :asc)
+    @search_settings = Sg::Setting.search_settings.active.order(id: :asc)
     get_parametrizer(params)
     
 
@@ -116,15 +117,40 @@ class WelcomeController < ApplicationController
 
   def get_parametrizer(parametrize)
     aux_active = false
-    @resultado = ""
+    @resultado = []
 
     aux_fields = @generic_searchs_settings.map {|f| f.title.parameterize.underscore.to_s }
     aux_fields.push('search')
 
     aux_fields.each do |f|
         if !parametrize[f.to_sym].blank?
-            aux_active = true
-            @resultado =  @resultado + (@resultado.blank? ? parametrize[f.to_sym] : "/#{parametrize[f.to_sym]}")
+          aux_active = true
+          search_aux = f.to_s=="search" ? Sg::Generic.search_type.first : @search_settings.select {|x| x.title.parameterize.underscore.to_s == f.to_s }[0]
+          sg_selects = search_aux.try(:sg_selects)
+          sg_tables = search_aux.try(:sg_table_fields)
+          tables_aux = ""
+          tables_array = []
+
+          if !sg_tables.blank?
+            sg_tables.each {|t| tables_array.push(t.table_name)}
+
+            tables_array.uniq.each {|a| tables_aux = tables_aux + (tables_aux.blank? ? a.singularize.classify.constantize.model_name.human : " / #{a.singularize.classify.constantize.model_name.human}")}                      
+          end
+
+         
+
+        
+          search_data_aux = parametrize[f.to_sym]
+          if !search_aux.try(:data_type).blank? 
+            case search_aux.try(:data_type).to_s
+            when "select"
+              search_data_aux = sg_selects.select {|x| x.value.to_s == parametrize[f.to_sym].to_s }[0].try(:key)
+            when "checkbox"
+              search_data_aux = parametrize[f.to_sym].to_s == "true" ? "Sí" : "No"
+            end
+          end 
+          
+          @resultado.push({tabla: tables_aux, search: search_data_aux, field: (f.to_s=="search" ? "Barra de búsqueda general" : search_aux.try(:title)), count: 0, id: f.to_sym})           
         end
     end
 
