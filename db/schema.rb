0f72cc3cfa58b4539b2714e33d32399477dbc718
Antonio Pérez <apcano1978@gmail.com>
2021-05-04 10:05:59 +0200

    hotfix

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20210420102437) do
-=======
-ActiveRecord::Schema.define(version: 20191220110217) do
->>>>>>> e4556ba79... PR3489 Module 19
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
