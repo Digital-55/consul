1afa48cd1e7be4fe7ec8a96b4bc431ffd06bf55b
Ruben Blazquez Garcia <rblazquezg@indra.es>
2021-05-12 09:03:13 +0200

    [DEMAD] Correcciones

--- app/models/import_user.rb
+++ app/models/import_user.rb
@@ -134,7 +134,7 @@ class ImportUser < BaseImporter
     end
 
     def build_user(row)
-      user = User.new(username: row[:usuario], email: row[:email], name: row[:nombre], last_name: row[:primer_apellido], 
+      user = User.new(username: row[:usuario], email: row[:email], first_name: row[:nombre], last_name: row[:primer_apellido], 
         last_name_alt: row[:segundo_apellido], phone_number: row[:telefono], document_number: row[:documento], 
         date_of_birth: row[:fecha_nacimiento], terms_of_service: "1", residence_verified_at: Time.current, 
         verified_at: Time.current)
