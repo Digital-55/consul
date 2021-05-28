9192c2b65d1a10ea64082fd0a1b906897bbe6b9f
Ruben Blazquez Garcia <rblazquezg@indra.es>
2021-05-28 10:25:07 +0200

    [DEMAD-127] Consejo Social
    Landing page consejo social

--- config/routes.rb
+++ config/routes.rb
@@ -37,19 +37,24 @@ Rails.application.routes.draw do
   draw :valuation
   draw :verification
   draw :sures
+  draw :csc
 
   root "welcome#index"
   get "/welcome", to: "welcome#welcome"
   get "/conectados", to: "welcome#encuentrosconexpertos"
   get '/update_padron' => 'users#update_padron', as: :update_padron
   get "/eventos", to: "welcome#eventos"  
-  get "/semana-administracion-abierta", to: "welcome#agend_admin" 
+  get "/semana-administracion-abierta", to: "welcome#agend_admin"
+  #get "/organos/consejosocial" to: "csc#index"
 
   get "/madridsalealbalcon" => redirect("/legislation/processes/#{Rails.application.secrets.id}")
   get "/compartimosbarrio" => redirect("/legislation/processes/#{Rails.application.secrets.id_barrio}")
   get "/reto-compartimos-barrio" => redirect("legislation/processes/#{Rails.application.secrets.id_reto_compartimos_barrio}/proposals")
   
   get "/consul.json", to: "installation#details"
+
+  get "localhost:3000/consejosocial.madrid.es", to: "csc#index"
+  get "/organos/consejosocial", to: "csc#index"
   
   resources :stats, only: [:index]
   resources :images, only: [:destroy]
