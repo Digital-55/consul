2ea68a6dacfe116a595b1b07bf97852861f628e6
Antonio Javier Moreno Hernández <ajmorenoh@indra.es>
2021-05-31 18:20:29 +0200

    [DEMAD-SG] Corrección filtro

--- app/models/legislation/process.rb
+++ app/models/legislation/process.rb
@@ -53,7 +53,7 @@ class Legislation::Process < ApplicationRecord
   validates :font_color, format: { allow_blank: true, with: CSS_HEX_COLOR }
 
   scope :open, -> { where("start_date <= ? and end_date >= ?", Date.current, Date.current) }
-  scope :seached, ->  where("start_date <= ?", Date.current) }
+  scope :seached, -> { where("start_date <= ?", Date.current) }
   scope :active, -> { where("end_date >= ?", Date.current) }
   scope :past, -> { where("end_date < ?", Date.current) }
   scope :by_geozone_id, ->(geozone_id) { where(geozones: {id: geozone_id}.joins(:geozones)) }
