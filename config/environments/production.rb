32e15d2bcde6ea2704ef15768426f4807d75c2b8
Antonio Javier Moreno Hernández <ajmorenoh@indra.es>
2021-06-09 11:52:32 +0200

    [DEMAD-137] Despliegue de configuración completa

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.smtp_settings = {
     address: Rails.application.secrets.server_name_mail,
+    port: 25,
+    domain: "munimadrid.es"
   }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
