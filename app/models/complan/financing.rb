6960b9573c150fe4c18d74c22fc58ba6bb8b30dd
Antonio Javier Moreno Hernández <ajmorenoh@indra.es>
2021-06-30 12:39:43 +0200

    [DEMAD-APP-H2] Desarrollo de formularios

--- app/models/complan/financing.rb
+++ app/models/complan/financing.rb
@@ -3,6 +3,12 @@ class Complan::Financing < ApplicationRecord
     has_many :complan_imports, foreign_key: "complan_financing_id", class_name: "Complan::Import", dependent: :destroy
     has_many :complan_performances, foreign_key: "complan_financing_id", class_name: "Complan::Performance", dependent: :destroy
 
+    accepts_nested_attributes_for :complan_files, allow_destroy: true
+    accepts_nested_attributes_for :complan_imports, allow_destroy: true
+
+    validates_associated :complan_files
+    validates_associated :complan_imports
+
     self.table_name = "complan_financings"
 
     def self.get_columns
